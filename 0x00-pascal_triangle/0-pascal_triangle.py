9ecae4180a1d432c2ab3cdfdddd9959f39d5bd2e
mbukeRepo <mbuke404@gmail.com>
2022-04-06 21:43:06 +0200

    corrected the pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    else:
+        line = []
+        prev = []
+        triangles = []
+        for i in range(0, n + 1):
+            line = [j > 0 and j < i - 1 and i > 2 and prev[j - 1] +
+                    prev[j] or 1 for j in range(0, i)]
+            prev = line
+            triangles += [line]
+        return triangles[1:]
