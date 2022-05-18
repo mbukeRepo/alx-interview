6a4211ef79dbddb17585a0a2a016d5ffd95ce822
mbukeRepo <mbuke404@gmail.com>
2022-05-18 20:52:16 +0200

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/** 
+   interacting with swapi api
+   script will display challacters of a movie given movie Id
+**/
+
+const request = require('request');
+const movieId = process.argv[2];
+const baseUrl = 'https://swapi-api.hbtn.io/api/';
+request(baseUrl + 'films/' + movieId, async (error, response, body) => {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      await new Promise((resolve, reject) => {
+        request(character, (error, response, body) => {
+          if (!error) {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
+      });
+    }
+  }
+});
