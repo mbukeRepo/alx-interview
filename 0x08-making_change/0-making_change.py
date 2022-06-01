571eb04ea220dda5b5785ab546a57f1f02c5d0dc
mbukeRepo <mbuke404@gmail.com>
2022-06-01 15:48:33 +0200

    implemented the solution with dynamic programming

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" implements coin changes algorithm"""
+
+def makeChange(coins, total):
+    """ Args: 
+        @coins: array of coins
+        @total: integer(amount to make changes to)
+    """
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for a in range(1, total + 1):
+        for c in coins:
+            if a - c >= 0:
+                dp[a] = min(dp[a], 1 + dp[a - c])
+
+    if dp[total] == total + 1:
+        return -1
+    return dp[total]
