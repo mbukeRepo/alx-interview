6da97bab5b8dbc092edad865f501b4ff10604615
mbukeRepo <mbuke404@gmail.com>
2022-06-08 19:03:39 +0200

    added implementation

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" this file contains implementation of calculation of island perimeter"""
+
+
+def dfs(i, j, grid, visit):
+    """helper function that implements perimeter calculation"""
+    if i >= len(grid) or j >= len(grid[0]) or \
+       i < 0 or j < 0 or grid[i][j] == 0:
+        return 1
+    if (i, j) in visit:
+        return 0
+    visit.add((i, j))
+    perim = dfs(i, j + 1, grid, visit)
+    perim += dfs(i + 1, j, grid, visit)
+    perim += dfs(i, j - 1, grid, visit)
+    perim += dfs(i - 1, j, grid, visit)
+
+    return perim
+
+
+def island_perimeter(grid):
+    """calculates island perimeter"""
+    visit = set()
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return dfs(i, j, grid, visit)
---
+++ 0x09-island_perimeter/test.py
@@ -0,0 +1,22 @@
+def dfs(i, j,grid, visit):
+    if i >= len(grid) or j >= len(grid[0]) or \
+       i < 0 or j < 0 or grid[i][j] == 0:
+        return 1
+    if (i, j) in visit:
+        return 0
+    visit.add((i,j))
+    perim = dfs(i, j + 1, grid, visit)
+    perim += dfs(i + 1, j, grid, visit)
+    perim += dfs(i, j - 1, grid, visit)
+    perim += dfs(i - 1, j, grid, visit)
+
+    return perim
+    
+
+def island_perimeter(grid):
+    visit = set()
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return dfs(i, j, grid, visit)
